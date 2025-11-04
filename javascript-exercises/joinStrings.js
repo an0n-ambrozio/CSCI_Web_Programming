3a61f42686a0dd780639402e365930825cd70ada
an0n-ambrozio <matheus@ambrozio.space>
2025-11-03 20:09:30 -0600

    Exercises solved

--- javascript-exercises/joinStrings.js
+++ javascript-exercises/joinStrings.js
@@ -1,15 +1,22 @@
 /*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+ First name: Matheus
+ Last name: Ambrozio
+ This year: 2025
+ Birth year: 2002
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+ The greeting should read: "Hello! My name is Matheus Abrozio and I am 23 years old."
 */
 
 // Add your code right below, good luck!
 
+const firstName = "Matheus";
+const lastName = "Ambrozio";
+const thisYear = 2025;
+const birthYear = 2002;
 
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 
